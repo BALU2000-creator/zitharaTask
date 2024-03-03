c1a47998f9eb8bfaf410f28f6e8e7496191f72e4
BALU2000-creator <82692839+BALU2000-creator@users.noreply.github.com>
2024-03-03 14:46:34 +0530

    Add files via upload

---
+++ zithara/src/mockData.js
@@ -0,0 +1,98 @@
+const mockData = [
+  {
+    sno: 1,
+    customer_name: "John Doe",
+    age: 25,
+    phone: "123-456-7890",
+    location: "New York",
+    created_at: "2024-02-29T10:30:00Z"
+  },
+  {
+    sno: 2,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+
+
+
+  // Add more mock data records
+  {
+    sno: 3,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+
+  {
+    sno: 4,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+  
+  {
+    sno: 5,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+  
+  {
+    sno: 6,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+  
+  {
+    sno: 7,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+  
+  {
+    sno: 8,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+  
+
+  {
+    sno: 9,
+    customer_name: "Jane Smith",
+    age: 30,
+    phone: "987-654-3210",
+    location: "Los Angeles",
+    created_at: "2024-02-28T12:45:00Z"
+  },
+  
+
+
+  {
+    sno: 10,
+    customer_name: "Alice Johnson",
+    age: 28,
+    phone: "555-555-5555",
+    location: "Chicago",
+    created_at: "2024-02-25T08:00:00Z"
+  }
+];
+
+export default mockData;
